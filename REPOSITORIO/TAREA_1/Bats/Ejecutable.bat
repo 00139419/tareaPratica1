cece4a158dd5a22ec7c702c6dead45beec8ebc96
00139419 <00139419@uca.edu.sv>
2023-10-09 10:12:35 -0700

    Se agrego: archivo zip del proyecto

--- REPOSITORIO/TAREA_1/Bats/Ejecutable.bat
+++ REPOSITORIO/TAREA_1/Bats/Ejecutable.bat
@@ -1,4 +1,4 @@
 @echo off
 setlocal
 cd "C:\Program Files (Arm)\pdi-ce-9.4.0.0-343\data-integration"
-start "Ejecutando ETL" /wait "C:\Program Files (Arm)\pdi-ce-9.4.0.0-343\data-integration\Spoon.bat" /file:"C:\Users\Daniel\Documents\Git\tareaPratica1\REPOSITORIO\TAREA_1\Jobs\J_Main.kjb"
+start "Ejecutando ETL" /wait "C:\BATS\Ejecutable.bat" /file:"C:\REPOSITORIO\TAREA_1\Jobs\J_Main.kjb"
